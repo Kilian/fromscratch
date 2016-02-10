f46f2aa1bfe05bb484907686a8c7b971c5cecdef
Kilian Valkhof <kilian@kilianvalkhof.com>
2016-02-10 21:55:02 +0100

    update menu template to work properly in OSX re #2

--- main.js
+++ main.js
@@ -103,40 +103,40 @@ app.on('ready', function() {
 
   if (process.platform === 'darwin') {
     var template = [{
-      'label': 'FromScratch',
-      'submenu': [{
+      label: 'FromScratch',
+      submenu: [{
         label: 'Quit',
         accelerator: 'CmdOrCtrl+Q',
         click: function() { app.quit(); }
       }]
     }, {
-      'label': 'Edit',
-      'submenu': [{
-        'label': 'Undo',
-        'accelerator': 'CmdOrCtrl+Z',
-        'selector': 'undo:'
+      label: 'Edit',
+      submenu: [{
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        selector: 'undo:'
       }, {
-        'label': 'Redo',
-        'accelerator': 'Shift+CmdOrCtrl+Z',
-        'selector': 'redo:'
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        selector: 'redo:'
       }, {
-        'type': 'separator'
+        type: 'separator'
       }, {
-        'label': 'Cut',
-        'accelerator': 'CmdOrCtrl+X',
-        'selector': 'cut:'
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        selector: 'cut:'
       }, {
-        'label': 'Copy',
-        'accelerator': 'CmdOrCtrl+C',
-        'selector': 'copy:'
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        selector: 'copy:'
       }, {
-        'label': 'Paste',
-        'accelerator': 'CmdOrCtrl+V',
-        'selector': 'paste'
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        selector: 'paste:'
       }, {
-        'label': 'Select All',
-        'accelerator': 'CmdOrCtrl+A',
-        'selector': 'selectAll:'
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        selector: 'selectAll:'
       }]
     }];
 
