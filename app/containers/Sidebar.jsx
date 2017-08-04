e7b955b2eee008219d0e697afd53e9261663275e
wthen <wojciech.then@nokia.pl>
2017-08-04 14:31:44 +0200

    minor adjustments, restructured Sidebar.jsx

--- app/containers/Sidebar.jsx
+++ app/containers/Sidebar.jsx
@@ -17,30 +17,87 @@ const utils         = remote.getGlobal('utils');
 let latestVersion;
 
 
-class ItemActions extends React.Component {
+export default class Sidebar extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super();
-
-        // pass onRename / onRemove callbacks through props - will change between project and file
-        // some kind of dialog for new name / confirm dialog
-
-        // somehow on hover show info what the control will do in some kind of popup / tooltip
+        this.state = {};
+        projects.refreshProjectsTree();
     }
 
+    render() {
+        let self = this;
 
-    render(){
+        let sidebarItems = Object.entries(projects.tree).map((item, i) => {
+            let key = (new Date).getTime() + ':' + i;
+            return (
+                <ProjectItem project={item[0]} scratches={item[1]} refreshScratch={self.props.refreshScratch} key={key} />
+            );
+        });
+
+        return (
+            <div className="sidebar">
+
+                <div className="new-project" id="new-project">
+                    <span className="new-project-label">Create new project</span>
+                    <Ionicon icon="ion-ios-plus-outline" fontSize="20px" className="sidebar-icon new-project-handle" />
+                    <span className="sidebar-title">Your projects</span>
+                </div>
+
+                <div className="sidebar-tree">
+                    {sidebarItems}
+                </div>
+
+            </div>
+        );
+    }
+
+}
 
 
-        return(
-            <span className="item-actions">
-                 <span className="item-action action-remove" title={'Remove ' + this.props.parentName}>
-                    <Ionicon icon="ion-ios-trash-outline" fontSize="20px" className="sidebar-icon" />
-                </span>
-                <span className="item-action action-rename" title={'Rename ' + this.props.parentName}>
-                    <Ionicon icon="ion-ios-at" fontSize="20px" className="sidebar-icon"/>
-                </span>
-            </span>
+class ProjectItem extends React.Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            open: false,  // if expanded (scratches inside are visible) or not
+            active: false // if one of the child scratches is the currently selected scratch
+        };
+        this.computedStyle = {};
+        this.parentClasses = ['project']
+    }
+
+    componentWillMount() {
+        let self = this;
+        this.scratches = this.props.scratches.map((scratch, i) => {
+            let key = (new Date).getTime() + ':' + i;
+            return (
+                <FileItem name={scratch} refreshScratch={self.props.refreshScratch} data={{ project: this.props.project, scratch: scratch }} key={key} />
+            );
+        });
+    }
+
+    onClick = (ev) => {
+        this.state.open = !this.state.open;
+        this.parentClasses = this.state.open ? utils.addClass(this.parentClasses, 'open') : utils.removeClass(this.parentClasses, 'open');
+        this.computedStyle = !this.state.open ? {} : {
+            maxHeight: (this.props.scratches.length * 26) + 'px'
+        };
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <div className={this.parentClasses.join(' ')}>
+                <div className="project-label" onClick={this.onClick}>
+                    <Ionicon icon="ion-ios-arrow-right" fontSize="20px" className="sidebar-icon project-label-icon" />
+                    <span className="label">{this.props.project}</span>
+                    <span className="actions"><ItemActions parentName="project" /></span>
+                </div>
+                <div className="file-items-container" style={this.computedStyle}>
+                    {this.scratches}
+                </div>
+            </div>
         );
     }
 }
@@ -56,7 +113,7 @@ class FileItem extends React.Component {
         signals.subscribe('adjust-file-item-state', this.onSignal);
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         signals.unsubscribe('adjust-file-item-state', this.onSignal);
     }
 
@@ -88,87 +145,24 @@ class FileItem extends React.Component {
 }
 
 
-class ProjectItem extends React.Component {
-
-    constructor(props){
-        super();
-        this.state = {
-            open: false,  // if expanded (scratches inside are visible) or not
-            active: false // if one of the child scratches is the currently selected scratch
-        };
-        this.computedStyle = {};
-        this.parentClasses = ['project']
-    }
-
-    componentWillMount(){
-        let self = this;
-        this.scratches = this.props.scratches.map((scratch, i) => {
-            let key = (new Date).getTime() + ':' + i;
-            return (
-                <FileItem name={scratch} refreshScratch={self.props.refreshScratch} data={{ project: this.props.project, scratch: scratch }} key={key} />
-            );
-        });
-    }
-
-    onClick = (ev) => {
-        this.state.open = !this.state.open;
-        this.parentClasses = this.state.open ? utils.addClass(this.parentClasses, 'open') : utils.removeClass(this.parentClasses, 'open');
-        this.computedStyle = !this.state.open ? {} : {
-            maxHeight: (this.props.scratches.length * 26) + 'px'
-        };
-        this.forceUpdate();
-    }
-
-    render(){
-        return (
-            <div className={this.parentClasses.join(' ')}>
-                <div className="project-label" onClick={this.onClick}>
-                    <Ionicon icon="ion-ios-arrow-right" fontSize="20px" className="sidebar-icon project-label-icon" />
-                    <span className="label">{this.props.project}</span>
-                    <span className="actions"><ItemActions parentName="project" /></span>
-                </div>
-                <div className="file-items-container" style={this.computedStyle}>
-                    {this.scratches}
-                </div>
-            </div>
-        );
-    }
-}
-
-
-export default class Sidebar extends React.Component {
+class ItemActions extends React.Component {
 
     constructor(props) {
         super();
-        this.state = {};
-        projects.refreshProjectsTree();
+        // pass onRename / onRemove callbacks through props - will change between project and file
+        // some kind of dialog for new name / confirm dialog
     }
 
     render() {
-        let self = this;
-
-        let sidebarItems = Object.entries(projects.tree).map((item, i) => {
-            let key = (new Date).getTime() + ':' + i;
-            return (
-                <ProjectItem project={item[0]} scratches={item[1]} refreshScratch={self.props.refreshScratch} key={this.key} />
-            );
-        });
-
         return (
-            <div className="sidebar">
-
-                <div className="new-project" id="new-project">
-                    <span className="new-project-label">Create new project</span>
-                    <Ionicon icon="ion-ios-plus-outline" fontSize="20px" className="sidebar-icon new-project-handle" />
-                    <span className="sidebar-title">Your projects</span>
-                </div>
-
-                <div className="sidebar-tree">
-                    {sidebarItems}
-                </div>
-
-            </div>
+            <span className="item-actions">
+                <span className="item-action action-remove" title={'Remove ' + this.props.parentName}>
+                    <Ionicon icon="ion-ios-trash-outline" fontSize="20px" className="sidebar-icon" />
+                </span>
+                <span className="item-action action-rename" title={'Rename ' + this.props.parentName}>
+                    <Ionicon icon="ion-ios-at" fontSize="20px" className="sidebar-icon" />
+                </span>
+            </span>
         );
     }
-
 }
